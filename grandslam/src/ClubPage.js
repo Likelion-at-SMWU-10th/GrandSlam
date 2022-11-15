38793c9bc095105cc1bb6b149eeefdd0189249e6
sumin <tnals8939@daum.net>
2022-11-15 18:19:09 +0900

    [K] 메인 페이지 아래 방향 화살표 구현

--- grandslam/src/ClubPage.js
+++ grandslam/src/ClubPage.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import styled, {css} from 'styled-components';
 
 const ClubPage = () => {
     return (
         <div>
-            this is club page
+            <Move direction='up' style={{color: '#ffffff', bgcolor:'#010C1F'}}>
+                <img src="/img/glandslam.png" width="50" height="814.88"></img>
+            </Move>
         </div>
     );
 };
+
+const Move = styled.marquee`
+    scrolldelay:5;
+    width:53px;
+    height:1000px;
+    loop:infinite;
+    overflow:hidden;
+`
 
 export default ClubPage;
