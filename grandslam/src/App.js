bcc0008b12455264d6cd543131f2a7e31611147b
jiyoon <giyun3632@sookmyung.ac.kr>
2022-11-20 18:21:32 +0900

    배경 추가

--- grandslam/src/App.js
+++ grandslam/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WrapDiv>
+            <Move direction='up' style={{color: '#ffffff', bgcolor:'#010C1F'}}>
+                <img src="/img/grandslam.png" width="50" height="814.88"></img>
+            </Move>
+      <KT_back src="/img/KT_back.png"/>
+      <Doosan_back src="/img/Doosan_back.png"/>
+      <Samsung_back src="/img/Samsung_back.png"/>
+      <LG_back src="/img/LG_back.png"/>
+      <Kiwoom_back src="/img/Kiwoom_back.png"/>
+      <SSG_back src="/img/SSG_back.png"/>
+      <NC_back src="/img/NC_back.png"/>
+      <Lotte_back src="/img/Lotte_back.png"/>
+      <KIA_back src="/img/KIA_back.png"/>
+      <Hanhwa_back src="/img/Hanhwa_back.png"/>
+      
+    </WrapDiv>
   );
 }
+
+const Move = styled.marquee`
+    position: absolute;
+    scrolldelay:5;
+    width:53px;
+    loop:infinite;
+`
+const WrapDiv = styled.div`
+  background: #ffffff;
+  font-size: 0px;
+`
+const Intro = styled.img`
+
+`
+
+const KT_back = styled.img`
+  width:100%;
+
+`
+
+const Doosan_back = styled.img`
+  width:100%;
+
+`
+
+const Samsung_back = styled.img`
+  width:100%;
+
+`
+const LG_back = styled.img`
+  width:100%;
+
+`
+const Kiwoom_back = styled.img`
+  width:100%;
+
+`
+const SSG_back = styled.img`
+  width:100%;
+
+`
+const NC_back = styled.img`
+  width:100%;
+
+`
+const KIA_back = styled.img`
+  width:100%;
+
+`
+const Lotte_back = styled.img`
+  width:100%;
+
+`
+const Hanhwa_back = styled.img`
+  width:100%;
+
+`
+
+
 
 export default App;
