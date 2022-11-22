810ce76804af3dc2bf4cb04b3f408b7f20f16e43
jiyoon <giyun3632@sookmyung.ac.kr>
2022-11-22 16:43:54 +0900

    깃

--- grandslam/src/MainPage.js
+++ grandslam/src/MainPage.js
@@ -3,20 +3,59 @@ import Music1 from "./Music1";
 import Music2 from "./Music2";
 import Music3 from "./Music3";
 import LP from "./LP";
+import { Link } from "react-scroll"
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [ScrollY, setScrollY] = useState(0);
+  const [bts, setBtnStatus] =  useState(0);
+
+  const handleTop = () => {  // 클릭하면 스크롤이 위로 올라가는 함수
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    setScrollY(0);
+    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
+  }
+
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+    if(ScrollY > 100) {
+      // 100 이상이면 버튼이 보이게
+      setBtnStatus(1);
+    } else {
+      // 100 이하면 버튼이 사라지게
+      setBtnStatus(0);
+    }
+  }
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow);
+    }
+    watch(); // addEventListener 함수를 실행
+    return () => {
+      window.removeEventListener('scroll', handleFollow); // addEventListener 함수를 삭제
+    }
+  })
+
   return (
     <>
       <Wrap>
-        <Music1></Music1>
-        <Music2></Music2>
-        <Music3></Music3>
+        <PText color={"#FCD789"} btnStatus={!bts}>GRANDSLAM</PText>
+        <TopBtn btnStatus = {bts}// 버튼 노출 여부
+          onClick={handleTop}  // 버튼 클릭시 함수 호출
+          >TOP</TopBtn>
         <WrapDiv>
+          <Music1></Music1>
+          <Music2></Music2>
+          <Music3></Music3> 
           <Move
             direction="down"
             scrolldelay="0"
             loop="infinite"
-            scrollamount="2"
+            scrollamount="2000"
             style={{ color: "#ffffff", bgcolor: "#010C1F" }}
           >
             <img src="/img/marquee2.svg" width="60" height="10000"></img>
@@ -45,6 +84,69 @@ function App() {
   );
 }
 
+const PText = styled.p`
+  position: fixed;
+  top: 0px;
+  left: 10px;
+  font-family: 'Mr Dafoe', cursive;
+  text-shadow: 5px 5px 3px #31748F;
+  text-align: center;
+  width: 100%;
+  font-size: 100px;
+  color: ${props=> props.color || '#FCD789'};
+  opacity: ${props=> props.btnStatus || 0 };
+  cursor: pointer;
+  transition: opacity 0.3s ease-in;
+`
+
+const Btns = styled.button`
+  position : fixed;
+  bottom: 40px; 
+  right: 100px;
+  
+  z-index: 10; 
+  width: 50px; 
+  height: 50px;
+  opacity: ${props=> props.btnStatus || 0 }
+  
+  z-index: 10; 
+  width: 50px; 
+  height: 50px;
+  border-radius: 100%;
+  border: 0 none;
+  background: black;
+  color: red;
+  border: 2px solid red;
+  font-size: 18px;
+  font-weight: bold;
+  letter-spacing: -0.06em;
+  box-shadow: 1px 1px 6px 3px rgba(0,0,0,0.3);
+  cursor: pointer;
+  transition: opacity 0.3s ease-in;
+`
+
+const TopBtn = styled.button`
+  position: fixed; 
+  opacity: ${props=> props.btnStatus || 0 };
+  bottom: 40px; 
+  right: 40px;
+  
+  z-index: 10; 
+  width: 50px; 
+  height: 50px;
+  border-radius: 100%;
+  border: 0 none;
+  background: black;
+  color: white;
+  border: 2px solid white;
+  font-size: 18px;
+  font-weight: bold;
+  letter-spacing: -0.06em;
+  box-shadow: 1px 1px 6px 3px rgba(0,0,0,0.3);
+  cursor: pointer;
+  transition: opacity 0.3s ease-in;
+`
+
 const Wrap = styled.div`
   position: relative;
 `;
@@ -52,6 +154,9 @@ const Wrap = styled.div`
 const Move = styled.marquee`
   position: absolute;
   width: 53px;
+  height:  100%;
+  background: white;
+  opacity: 0.8;
 `;
 const WrapDiv = styled.div`
   background: #ffffff;
